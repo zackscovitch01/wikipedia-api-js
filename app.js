4384fba93105e8d312694406247c266ada38e838
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-13 06:49:09 -0400

    renderResults()

--- app.js
+++ app.js
@@ -14,4 +14,33 @@ formDOM.addEventListener("submit", (e) => {
   fetchPages(value);
 });
 
-const fetchPages = async (searchValue) => {};
+const fetchPages = async (searchValue) => {
+  resultsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultsDOM.innerHTML = `<div class="error"> no matching results, please try again...</div>`;
+      return;
+    }
+    renderResults(results);
+  } catch (error) {
+    resultsDOM.innerHTML = `<div class="error"> there was an error...</div>`;
+  }
+};
+
+const renderResults = (list) => {
+  const cardList = list
+    .map((card) => {
+      const { title, snippet, pageid } = card;
+      return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>`;
+    })
+    .join();
+  resultsDOM.innerHTML = `<div class="articles">${cardList}</div>`;
+};
